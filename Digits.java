f04ac4b4b17f823a64d64014fa12798ee9ab7136
master-of-fire <99187679+master-of-fire@users.noreply.github.com>
2025-02-26 09:53:31 -0500

    Update Digits.java

---
+++ Digits.java
@@ -0,0 +1,22 @@
+/************************************
+*Project: Digits
+*Programmer: Zaid Ahmad
+*Date: Feb 25, 2025
+*Program Name: Digits.java
+*************************************/
+public class Digits {
+    public static void main(String[]args) {
+    int number;
+    int ones;
+    int tens;
+    int hundreds;
+    int thousanths;
+    number= 2589;
+    ones= number%10;
+    tens= (number/10)%10;
+    hundreds= ((number/10)/10)%10;
+    thousanths= (((number/10)/10)/10)%10;
+    System.out.println("the digit in the ones place is "+ones+". the digit in the tens place is "+tens+". the digit in the hundreds place is "+hundreds+". and the digit in the thousanths place is "+thousanths);
+}
+
+}
