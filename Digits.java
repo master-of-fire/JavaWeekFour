85ccc1618bb56c19985c17a45d1824749c203253
master-of-fire <99187679+master-of-fire@users.noreply.github.com>
2025-02-26 10:26:07 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -4,6 +4,7 @@
 *Date: Feb 25, 2025
 *Program Name: Digits.java
 *************************************/
+import java.util.*;
 public class Digits {
     public static void main(String[]args) {
     int number;
@@ -11,12 +12,19 @@ public class Digits {
     int tens;
     int hundreds;
     int thousanths;
-    number= 2589;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a 4 digit number");
+    number= input.nextInt();
+    //number= 2589;
     ones= number%10;
     tens= (number/10)%10;
     hundreds= ((number/10)/10)%10;
     thousanths= (((number/10)/10)/10)%10;
     System.out.println("the digit in the ones place is "+ones+". the digit in the tens place is "+tens+". the digit in the hundreds place is "+hundreds+". and the digit in the thousanths place is "+thousanths);
-}
+    
+    
+    
 
 }
+}
+
